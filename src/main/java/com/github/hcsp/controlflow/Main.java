321f90d9202a057ee6b2e0cdac518f106f93032d
7952126 <15521608275@163.com>
2020-03-27 12:45:28 +0800

    3

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -20,25 +20,29 @@ public class Main {
      * @return 结果字符串
      */
     public static String howManyDaysInMonth(int month) {
-        switch (month) {
-            case 1:
-            case 3:
-            case 5:
-            case 7:
-            case 8:
-            case 10:
-            case 12:
-                System.out.println(month + "月有31天");
-                break;
-            case 2:
-                System.out.println(month + "月有28天");
-                break;
-            case 4:
-            case 6:
-            case 9:
-            case 11:
-                System.out.println(month + "月有30天");
-                break;
+        if (month < 1 || month > 12) {
+            return "非法输入";
+        } else {
+            switch (month) {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    System.out.println(month + "月有31天");
+                    break;
+                case 2:
+                    System.out.println(month + "月有28天");
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    System.out.println(month + "月有30天");
+                    break;
+            }
         }
         return "";
     }

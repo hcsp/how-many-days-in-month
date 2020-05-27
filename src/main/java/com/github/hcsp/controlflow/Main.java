3fc153cc58461a20ff2b3474351b9673a5a5816d
lazzben <458861463@qq.com>
2020-05-27 18:26:25 +0800

    判断一个平年的月份有多少天

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -20,6 +20,25 @@ public class Main {
      * @return 结果字符串
      */
     public static String howManyDaysInMonth(int month) {
-        return "";
+        switch (month) {
+            case 2:
+                return "28";
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                return "31";
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                return "30";
+            default:
+                return "非法输入";
+
+        }
     }
 }

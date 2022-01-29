9f2b7f229aea89ce4d716413e551895177c9aaf9
DDB324 <jiang128193895>
2022-01-29 14:07:01 +0800

    fix bug

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -26,15 +26,15 @@ public class Main {
             case 3:
             case 5:
             case 7:
-            case 9:
-            case 11:
+            case 8:
+            case 10:
+            case 12:
                 days = 31;
                 break;
             case 4:
             case 6:
-            case 8:
-            case 10:
-            case 12:
+            case 9:
+            case 11:
                 days = 30;
                 break;
             case 2:

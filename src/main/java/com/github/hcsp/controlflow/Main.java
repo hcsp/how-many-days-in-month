55e5fcef9628a14ef0101b2794378c0b69d86133
zj <woaiwrf@qq.com>
2022-06-19 19:28:00 +0800

    判断月份的天数

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -20,17 +20,20 @@ public class Main {
      * @return 结果字符串
      */
     public static String howManyDaysInMonth(int month) {
-        int days = switch (month) {
-            case 1, 3, 5, 7, 8, 10, 12 -> 31;
-            case 4, 6, 9, 11 -> 30;
-            case 2 -> 28;
-            default -> 0;
-        };
-        if (days == 0) {
-            return "非法输入";
-        } else {
-            return month + "月有" + days + "天";
+        int days = 0;
+        switch (month){
+            case 1,3,5,7,8,10,12:
+                days = 31;
+                break;
+            case 4,6,9,11:
+                days = 30;
+                break;
+            case 2:
+                days = 28;
+                break;
+            default:
+                return "非法输入";
         }
-
+        return String.format("%d月有%d天",month,days);
     }
 }

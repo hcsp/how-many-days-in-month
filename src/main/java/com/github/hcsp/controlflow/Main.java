4c3fcec752691d36f0347b6346a62a4847e7ddf7
wuzhenquan <782754354@qq.com>
2023-05-14 21:01:33 +0800

    feat: 根据月份判断有多少天 (#135)
    feat: 根据月份判断有多少天 (#135)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -20,6 +20,13 @@ public class Main {
      * @return 结果字符串
      */
     public static String howManyDaysInMonth(int month) {
-        return "";
+        if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+            return month + "月有31天";
+        } else if(month == 2) {
+            return month + "月有28天";
+        } else if(month == 4 || month == 6 || month == 9 || month == 11) {
+            return month + "月有30天";
+        }
+        return "非法输入";
     }
 }
